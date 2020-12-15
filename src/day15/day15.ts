9f083ff623b85c00f96490cad45ca69e88838af0
Colin Grodecoeur <colin.grodecoeur@360learning.com>
2020-12-15 13:05:51 +0100

    Day 15 Improve performance (<3s for part 2)

--- src/day15/day15.ts
+++ src/day15/day15.ts
@@ -8,8 +8,7 @@ export function day15() {
 
     return {
         part1: () => part1(numbers),
-        // Skip part 2 as it's too slow for now
-        // part2: () => part2(numbers)
+        part2: () => part2(numbers)
     };
 }
 
@@ -26,24 +25,20 @@ export function part2(numbers: number[]) {
 }
 
 function compute(numbers: number[], position: number) {
-    const v: Record<number, number> = {};
-    const w: Record<number, number> = {};
-    numbers.forEach((number, index) => {
-        v[number] = index;
-    });
-    let [, current] = numbers;
+    const v: (number | undefined)[] = _.range(position).map(() => undefined);
+    const w: (number | undefined)[] = _.range(position).map(() => undefined);
+    numbers.forEach((number, index) => v[number] = index);
+    let current = _.last(numbers)!;
 
     let n = numbers.length;
     while (n < position) {
         if (_.isUndefined(w[current])) {
             current = 0;
-            w[0] = v[0];
-            v[0] = n;
         } else {
-            current = n - 1 - w[current];
-            w[current] = v[current];
-            v[current] = n;
+            current = n - 1 - w[current]!;
         }
+        w[current] = v[current];
+        v[current] = n;
         n++;
     }
     return current;
--- src/day15/day15.unit.ts
+++ src/day15/day15.unit.ts
@@ -2,23 +2,36 @@ import { expect } from "@test/unit";
 import { parseInput, part1, part2 } from "./day15";
 
 describe("day15", () => {
-    it("part 1", () => {
-        expect(part1(parseInput("0,3,6"))).to.equal(436);
-        expect(part1(parseInput("1,3,2"))).to.equal(1);
-        expect(part1(parseInput("2,1,3"))).to.equal(10);
-        expect(part1(parseInput("1,2,3"))).to.equal(27);
-        expect(part1(parseInput("2,3,1"))).to.equal(78);
-        expect(part1(parseInput("3,2,1"))).to.equal(438);
-        expect(part1(parseInput("3,1,2"))).to.equal(1836);
+    describe("part 1", () => {
+        const testCases = {
+            "0,3,6": 436,
+            "1,3,2": 1,
+            "2,1,3": 10,
+            "1,2,3": 27,
+            "2,3,1": 78,
+            "3,2,1": 438,
+            "3,1,2": 1836
+        };
+        Object.entries(testCases).forEach(([input, expectedValue]) => {
+            it(`should work with the input ${input}`, () => {
+                expect(part1(parseInput(input))).to.equal(expectedValue);
+            });
+        });
     });
-    it.skip("part 2", () => {
-        // Too slow for now
-        expect(part2(parseInput("0,3,6"))).to.equal(175594);
-        expect(part2(parseInput("1,3,2"))).to.equal(2578);
-        expect(part2(parseInput("2,1,3"))).to.equal(3544142);
-        expect(part2(parseInput("1,2,3"))).to.equal(261214);
-        expect(part2(parseInput("2,3,1"))).to.equal(6895259);
-        expect(part2(parseInput("3,2,1"))).to.equal(18);
-        expect(part2(parseInput("3,1,2"))).to.equal(362);
+    describe("part 2", () => {
+        const testCases = {
+            "0,3,6": 175594,
+            "1,3,2": 2578,
+            "2,1,3": 3544142,
+            "1,2,3": 261214,
+            "2,3,1": 6895259,
+            "3,2,1": 18,
+            "3,1,2": 362
+        };
+        Object.entries(testCases).forEach(([input, expectedValue]) => {
+            it(`should work with the input ${input}`, () => {
+                expect(part2(parseInput(input))).to.equal(expectedValue);
+            }).timeout(5000);
+        });
     });
 });
